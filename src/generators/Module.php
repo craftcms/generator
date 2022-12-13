8e0481a5f1771f8bcfc29a7e5ece863eb946665c
brandonkelly <brandon@pixelandtonic.com>
2022-12-13 05:36:44 -0800

    Update config/app.php automatically

--- src/generators/Module.php
+++ src/generators/Module.php
@@ -9,7 +9,16 @@ namespace craft\generator\generators;
 
 use Craft;
 use craft\generator\BaseGenerator;
+use craft\generator\NodeVisitor;
+use craft\generator\Workspace;
 use Nette\PhpGenerator\PhpFile;
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\NodeTraverser;
 use yii\base\Module as YiiModule;
 
 /**
@@ -22,6 +31,7 @@ class Module extends BaseGenerator
     private string $id;
     private string $targetDir;
     private string $rootNamespace;
+    private bool $bootstrap;
 
     public function run(): bool
     {
@@ -34,6 +44,8 @@ class Module extends BaseGenerator
             'ensureEmpty' => true,
         ]);
 
+        $this->bootstrap = $this->command->confirm('Should the module be loaded during app initialization?');
+
         if (!file_exists($this->targetDir)) {
             $this->command->createDirectory($this->targetDir);
         }
@@ -41,39 +53,77 @@ class Module extends BaseGenerator
         // Module class
         $this->writeModuleClass();
 
-        $message = <<<MD
-**Module created!**
+        $message = '**Module created!**';
 
-MD;
         if ($addedRoot) {
-            $message .= <<<MD
+            $message .= "\n\n" . <<<MD
 Run the following command to ensure the module gets autoloaded:
 
 ```php
 > composer dump-autoload
 ```
-
 MD;
         }
 
-        $message .= <<<MD
-To install the module, open `config/app.php` and add the following to the `return` array:
+        $appConfigPath = Craft::$app->getConfig()->getConfigFilePath('app');
 
-```
+        if (!$this->modifyFile($appConfigPath, function(Workspace $workspace) {
+            $moduleClassName = $workspace->importClass("$this->rootNamespace\\Module");
+
+            if (!$workspace->modifyCode(new NodeVisitor(
+                enterNode: function(Node $node) use ($workspace, $moduleClassName) {
+                    if (!$node instanceof Return_) {
+                        return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+                    }
+                    // Make sure it an array is returned
+                    if (!$node->expr instanceof Array_) {
+                        return NodeTraverser::STOP_TRAVERSAL;
+                    }
+                    $workspace->mergeIntoArray($node->expr, [
+                        'modules' => [
+                            $this->id => new ClassConstFetch(new Name($moduleClassName), 'class'),
+                        ],
+                    ]);
+                    if ($this->bootstrap) {
+                        $workspace->mergeIntoArray($node->expr, [
+                            'bootstrap' => [
+                                new String_($this->id),
+                            ],
+                        ]);
+                    }
+                    return NodeTraverser::STOP_TRAVERSAL;
+                }
+            ))) {
+                return false;
+            }
+
+            return true;
+        })) {
+            $fallbackExample = <<<MD
 'modules' => [
     '$this->id' => \\$this->rootNamespace\\Module::class,
 ],
-```
-
-If you want your module to be loaded during application initialization on every request,
-also include `'$this->id'` in the `bootstrap` array:
-
-```
+MD;
+            if ($this->bootstrap) {
+                $fallbackExample .= "\n" . <<<MD
 'bootstrap' => [
     '$this->id',
 ],
-```
 MD;
+            }
+
+            $message .= "\n\n" . sprintf(
+                <<<'MD'
+To install%s the module, open `config/app.php` and add the following to the `return` array:
+
+```
+%s
+```
+MD,
+                $this->bootstrap ? ' and bootstrap' : '',
+                $fallbackExample,
+            );
+        }
         $this->command->success($message);
         return true;
     }
